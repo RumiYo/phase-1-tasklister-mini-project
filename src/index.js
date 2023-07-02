78ef38cfc25c7a4e74d6f759d869eba50a71b41f
RumiYo <yokoyamarumi@gmail.com>
2023-07-02 13:14:35 -0400

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    buildToDo(e.target["new-task-description"].value);
+    form.reset();
+  });
+  
+  function buildToDo(todo){
+    const li = document.createElement('li');
+    const btn = document.createElement('button');
+    btn.addEventListener('click', handleDelete);
+    btn.textContent = 'x';
+    li.textContent = `${todo} `;
+    li.appendChild(btn);
+    document.querySelector('#tasks').appendChild(li);
+   };
+
+function handleDelete(e){
+  e.target.parentNode.remove();
+}
+
 });
